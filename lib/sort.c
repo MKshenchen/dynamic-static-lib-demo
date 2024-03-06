8c7cbeba1d9108a2367175a05197a3f7f8ea16fc
MKshenchen <142572843+MKshenchen@users.noreply.github.com>
2024-03-06 13:40:05 +0800

    fix sort.c

--- lib/sort.c
+++ lib/sort.c
@@ -8,20 +8,19 @@ int partition(int arr[], int low, int high) {
   int pivot = arr[high];
   int i = (low - 1);
 
-  for (int j = low; j <= high - 1; j++) {
-    if (arr[j] < pivot) {
+  for (int j = low; j < high; j++) {
+    if (arr[j] < pivot)
       swap(&arr[++i], &arr[j]);
-    }
   }
   swap(&arr[i + 1], &arr[high]);
-  return (i + 1);
+  return i + 1;
 }
 
 void quickSort(int arr[], int low, int high) {
   if (low < high) {
-    int pi = partition(arr, low, high);
+    int pivot = partition(arr, low, high);
 
-    quickSort(arr, low, pi - 1);
-    quickSort(arr, pi + 1, high);
+    quickSort(arr, low, pivot - 1);
+    quickSort(arr, pivot + 1, high);
   }
 }
